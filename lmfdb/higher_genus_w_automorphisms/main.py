df90d550b9773af231bf72bb1be9afdcb0caa1f0
Edgar Costa <edgarc@mit.edu>
2019-11-10 17:56:27 -0500

    hgwa random

--- lmfdb/higher_genus_w_automorphisms/main.py
+++ lmfdb/higher_genus_w_automorphisms/main.py
@@ -451,6 +451,7 @@ def hgcwa_code_download_search(info):
             'download': hgcwa_code_download_search },
         cleaners={'signature': lambda field: ast.literal_eval(field['signature'])},
         bread=lambda: get_bread([("Search Results",'')]),
+        url_for_label=lambda label: url_for(".by_passport_label", passport_label=label),
         credit=lambda: credit)
 def higher_genus_w_automorphisms_search(info, query):
     if info.get('signature'):
