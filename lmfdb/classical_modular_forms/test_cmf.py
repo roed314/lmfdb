6a098c946d81e15224233293851de9493b4e7832
David Roe <roed.math@gmail.com>
2026-04-14 11:11:25 -0400

    Try using external link tester

--- lmfdb/classical_modular_forms/test_cmf.py
+++ lmfdb/classical_modular_forms/test_cmf.py
@@ -575,7 +575,8 @@ class CmfTest(LmfdbTest):
         self.check_args('/ModularForm/GL2/Q/holomorphic/mf_hecke_cc/', ['14,417,694', 'N.k.a.x.n.i'])
         self.check_args('/ModularForm/GL2/Q/holomorphic/mf_hecke_cc/?k=12', ['Select a', '269'])
         self.check_args('/ModularForm/GL2/Q/holomorphic/mf_hecke_cc/?N=12', ['Select a', '57'])
-        self.check_args('/ModularForm/GL2/Q/holomorphic/mf_hecke_cc/?N=11&k=2', '11.2.a.a.1.1') # Downloads from beta
+        path = '/ModularForm/GL2/Q/holomorphic/mf_hecke_cc/?N=11&k=2'
+        self.check_external(path, path, '11.2.a.a.1.1') # Downloads from beta
 
     def test_underlying_data(self):
         data = self.tc.get('/ModularForm/GL2/Q/holomorphic/data/13.2').get_data(as_text=True)
